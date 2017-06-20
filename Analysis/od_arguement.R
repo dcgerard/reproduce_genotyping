74deddc6c9de4a841f248814600c25c0f12c0047
David Gerard <gerard.1787@gmail.com>
2017-06-20 11:10:22 -0500

    od arguement

---
+++ Analysis/od_arguement.R
@@ -0,0 +1,87 @@
+## Argument for overdispersion
+library(updog)
+library(tidyverse)
+osize <- read.csv("./Output/shirasawa_snps/example_readcounts.csv", row.names = 1)[, 1]
+ocounts <- read.csv("./Output/shirasawa_snps/example_refcounts.csv", row.names = 1)[, 1]
+
+## Remove the counts close to 1 -----------------------------
+which_keep <- ocounts / osize < .99
+ocounts <- ocounts[which_keep]
+osize <- osize[which_keep]
+
+## Calculate intervals --------------------------------------
+maxn <- max(ocounts) * 2
+ploidy <- 6
+
+possible_probs <- 0:ploidy / ploidy
+
+## intervals at genotype = 5 ---------------------------------
+lower_seq5 <- stats::qbinom(p = 0.025, size = 1:maxn,
+    prob = possible_probs[ploidy])
+upper_seq5 <- stats::qbinom(p = 0.975, size = 1:maxn,
+    prob = possible_probs[ploidy])
+xseq_lower5 <- 1:maxn - lower_seq5
+xseq_upper5 <- 1:maxn - upper_seq5
+dat5 <- data_frame(Alower = lower_seq5, alower = xseq_lower5,
+    Aupper = upper_seq5, aupper = xseq_upper5)
+
+## intervals at genotype = 4 ----------------------------------
+lower_seq4 <- stats::qbinom(p = 0.025, size = 1:maxn,
+    prob = possible_probs[ploidy - 1])
+upper_seq4 <- stats::qbinom(p = 0.975, size = 1:maxn,
+    prob = possible_probs[ploidy - 1])
+xseq_lower4 <- 1:maxn - lower_seq4
+xseq_upper4 <- 1:maxn - upper_seq4
+dat4 <- data_frame(Alower = lower_seq4, alower = xseq_lower4,
+    Aupper = upper_seq4, aupper = xseq_upper4)
+
+## Find bad counts -------------------------------------------
+bad_ocounts <- (ocounts > dat5$Aupper[osize]) |
+               (ocounts < dat5$Alower[osize] &
+                ocounts > dat4$Aupper[osize]) |
+               (ocounts < dat4$Alower[osize])
+
+## make plot
+slopevec <- possible_probs / (1 - possible_probs)
+maxcount <- max(c(ocounts, osize - ocounts))
+xend <- pmin(rep(maxcount, ploidy + 1), maxcount / slopevec)
+yend <- pmin(rep(maxcount, ploidy + 1), maxcount * slopevec)
+df_lines <- data.frame(x = rep(0, ploidy + 1), y = rep(0,
+        ploidy + 1), xend = xend, yend = yend)
+
+dat_counts <- data_frame(A = ocounts, a = osize - ocounts, bad = bad_ocounts)
+
+pl <- ggplot(data = dat_counts, mapping = aes(x = a, y = A, col = bad_ocounts)) +
+    geom_point(size = 0.5) +
+    theme_bw() +
+    xlim(0, maxcount) +
+    ylim(0, maxcount) +
+    ylab("Counts A") +
+    xlab("Counts a") +
+    geom_segment(data = df_lines,
+        mapping = ggplot2::aes_string(x = "x", y = "y", xend = "xend",
+        yend = "yend"), lty = 2, alpha = 1 / 2, color = "black",
+        size = 0.5) +
+    ggthemes::scale_color_colorblind(guide = FALSE)
+print(pl)    
+
+colvec <- ggthemes::colorblind_pal()(4)
+
+pl <- pl + geom_line(data = dat5, mapping = aes(x = alower, y = Alower),
+    color = colvec[3], lty = 1, alpha = 1 / 2)
+pl <- pl + geom_line(data = dat5, mapping = aes(x = aupper, y = Aupper),
+    color = colvec[3], lty = 1, alpha = 1 / 2)
+pl <- pl + geom_line(data = dat4, mapping = aes(x = alower, y = Alower),
+    color = colvec[4], lty = 1, alpha = 1 / 2)
+pl <- pl + geom_line(data = dat4, mapping = aes(x = aupper, y = Aupper),
+    color = colvec[4], lty = 1, alpha = 1 / 2)
+
+pdf(file = "./Output/fig/od_arg.pdf", colormodel = "cmyk",
+    family = "Times", height = 3, width = 3)
+print(pl)
+dev.off()
+
+## Calculate p-value of seeing so many points outside the 95 percent intervals
+mean(bad_ocounts)
+pvalue <- stats::pbinom(q = sum(bad_ocounts), size = length(bad_ocounts), prob = 0.05, lower.tail = FALSE)
+pvalue
---
+++ Analysis/plot_quantiles.R
@@ -0,0 +1,50 @@
+## Plot 0.025, 0.5, and 0.975 quantiles of prior on sequencing error
+## rate and bias parameter.
+
+library(updog)
+library(tidyverse)
+ploidy <- 6
+
+bias_lower <- exp(stats::qnorm(0.025, mean = 0, sd = 1))
+bias_upper <- exp(stats::qnorm(0.975, mean = 0, sd = 1))
+
+seq_error_lower  <- updog:::expit(stats::qnorm(0.025, mean = -4.7, sd = 1))
+seq_error_median <- updog:::expit(-4.7)
+seq_error_upper <- updog:::expit(stats::qnorm(0.975, mean = -4.7, sd = 1))
+
+get_df_lines <- function(seq_error, bias_val, ploidy) {
+    porig <- as.factor(paste0(0:ploidy, "/", ploidy))
+    pvec <- updog::get_pvec(ploidy = ploidy, bias_val = bias_val,
+        seq_error = seq_error)
+    slopevec <- pvec / (1 - pvec)
+    xend <- pmin(rep(1, ploidy + 1), 1 / slopevec)
+    yend <- pmin(rep(1, ploidy + 1), 1 * slopevec)
+    df_lines <- data.frame(x = rep(0, ploidy + 1), y = rep(0,
+        ploidy + 1), xend = xend, yend = yend, porig = porig)
+    df_lines$bias <- format(bias_val, digits = 2)
+    df_lines$seq_error <- format(seq_error, digits = 2)
+    return(df_lines)
+}
+
+df_tot <- bind_rows(get_df_lines(seq_error = seq_error_upper, bias_val = 1, ploidy = 6),
+    get_df_lines(seq_error = 0.01, bias_val = bias_lower, ploidy = 6),
+    get_df_lines(seq_error = 0.01, bias_val = bias_upper, ploidy = 6))
+df_tot$bias_seq <- paste0("(", df_tot$bias, ", ", df_tot$seq_error, ")")
+df_tot$description <- c(rep("Seq Upper", ploidy + 1),
+                        rep("Bias Lower", ploidy + 1),
+                        rep("Bias Upper", ploidy + 1))
+
+pl <- ggplot(data = df_tot, mapping = aes(x = x, y = y, xend = xend, yend = yend, col = porig)) +
+    facet_grid(. ~ description) +
+    geom_segment() +
+    theme_bw() +
+    theme(strip.background = element_rect(fill = "white"),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank()) +
+        ggthemes::scale_color_colorblind(name = "Original\nProbabilities")
+
+pdf(file = "./Output/fig/prior_quantiles.pdf", colormodel = "cmyk",
+    family = "Times", height = 2.1, width = 6.5)
+print(pl)
+dev.off()
