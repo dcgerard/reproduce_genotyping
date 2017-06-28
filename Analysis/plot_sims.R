07b2db41b3575d9f1e61b00fbad4fc8d5e626dc3
David Gerard <gerard.1787@gmail.com>
2017-06-28 10:56:17 -0500

    plots

--- Analysis/plot_sims.R
+++ Analysis/plot_sims.R
@@ -24,7 +24,6 @@ pl <- ggplot(data = longdat, mapping = aes(y = PropCorrect, x = allele_freq, col
   facet_grid(bias_val ~ od_param) +
   theme_bw() +
   theme(strip.background = element_rect(fill = "white")) +
-  geom_abline(intercept = 0, slope = 1, lty = 2, alpha = 1/2) +
   xlab("Allele Frequency") +
   ylab("Proportion Correct") +
   geom_point(size = 0.1) +
@@ -92,27 +91,27 @@ pl_od <- ggplot(data = longdat, mapping = aes(x = as.factor(od_param), y = uod_p
 
 
 ## Bias estimates
-pl <- ggplot(data = longdat, mapping = aes(x = allele_freq, y = ubias_val)) +
+pl <- ggplot(data = longdat, mapping = aes(x = allele_freq, y = log2(ubias_val))) +
   facet_grid(bias_val ~ od_param) +
   theme_bw() +
   theme(strip.background = element_rect(fill = "white")) +
   xlab("Allele Frequency") +
-  ylab("Estimated Bias Parameter") +
+  ylab("Log2 Estimated Bias Parameter") +
   geom_point(size = 0.1) +
-  geom_hline(mapping = aes(yintercept = bias_val), lty = 2, color = "red")
+  geom_hline(mapping = aes(yintercept = log2(bias_val)), lty = 2, color = "red")
 pdf(file = "./Output/fig/bias_v_af.pdf", family = "Times", colormodel = "cmyk", height = 8, width = 6.5)
 print(pl)
 dev.off()
 
-pl_bias <- ggplot(data = longdat, mapping = aes(x = as.factor(od_param), y = ubias_val)) +
+pl_bias <- ggplot(data = longdat, mapping = aes(x = as.factor(od_param), y = log2(ubias_val))) +
   facet_grid(.~bias_val) +
   geom_boxplot(outlier.size = 0.1) +
   theme_bw() +
   theme(strip.background = element_rect(fill = "white"),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   xlab(expression(tau)) +
-  ylab(expression(hat(h))) +
-  geom_hline(mapping = aes(yintercept = bias_val), lty = 2, color = "red")
+  ylab(expression(log[2](hat(h)))) +
+  geom_hline(mapping = aes(yintercept = log2(bias_val)), lty = 2, color = "red")
 
 
 ## Sequencing error rate
